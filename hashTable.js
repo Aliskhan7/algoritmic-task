aeb1ef3b7b0373dd3acc60331e75308f847da6d0
aliskhangazamatov <ag@farcana.com>
2024-05-17 21:56:36 +0300

    hash table

---
+++ hashTable.js
@@ -0,0 +1,27 @@
+class HashTable {
+    store = new Array(10)
+    hash(key){
+        let sum = 0;
+        for (let i = 0; i< key.length; i++){
+            sum += key.charCodeAt(i)
+        }
+        return sum;
+    }
+
+    //O(1)
+    add(key, value){
+        this.store[this.hash(key)] = this.store[this.hash(key)] || []
+        this.store[this.hash(key)].push({key, value})
+    }
+    get(key){
+        return this.store[this.hash(key)].find((item) => item.key === key).value
+    }
+}
+
+const dict = new HashTable()
+//const dict = new Map()
+
+dict.add('aa', '3')
+dict.add('bb', '34')
+
+console.log(dict.get('aa'))
